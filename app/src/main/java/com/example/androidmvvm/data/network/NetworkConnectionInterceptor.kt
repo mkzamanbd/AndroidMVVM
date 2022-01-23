a726a031ecd922413e889edf5871e30750a87aac
Kamruzzaman <zaman7u@gmail.com>
2022-01-24 00:17:10 +0600

    update NetworkConnectionInterceptor and retrofit client

--- app/src/main/java/com/example/androidmvvm/data/network/NetworkConnectionInterceptor.kt
+++ app/src/main/java/com/example/androidmvvm/data/network/NetworkConnectionInterceptor.kt
@@ -1,7 +1,11 @@
 package com.example.androidmvvm.data.network
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.androidmvvm.BuildConfig
 import com.example.androidmvvm.utils.NoInternetException
 import okhttp3.Interceptor
@@ -18,16 +22,26 @@ class NetworkConnectionInterceptor(
         }
         return chain.proceed(chain.request().newBuilder().also {
             it.addHeader("Accept", "application/json")
+            it.addHeader("Content-Type", "application/x-www-form-urlencoded")
         }.build())
     }
 
+    @SuppressLint("NewApi")
     private fun isInternetAvailable(): Boolean {
+        var result = false
         val connectivityManager =
-            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        connectivityManager.activeNetworkInfo.also {
-            return it != null && it.isConnected
-        }
+            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
 
+        connectivityManager?.let {
+            it.getNetworkCapabilities(connectivityManager.activeNetwork)?.apply {
+                result = when {
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    else -> false
+                }
+            }
+        }
+        return result
     }
 
 }
--- app/src/main/java/com/example/androidmvvm/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/androidmvvm/data/network/RetrofitClient.kt
@@ -2,6 +2,8 @@ package com.example.androidmvvm.data.network
 
 import com.example.androidmvvm.BuildConfig
 import com.example.androidmvvm.data.response.AuthResponse
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.Authenticator
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -11,6 +13,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+import java.util.concurrent.TimeUnit
 
 interface RetrofitClient {
 
@@ -20,6 +23,7 @@ interface RetrofitClient {
         @Field("email") email: String,
         @Field("password") password: String,
     ): Response<AuthResponse>
+
     @FormUrlEncoded
     @POST("auth/register")
     suspend fun register(
@@ -34,7 +38,12 @@ interface RetrofitClient {
             networkConnectionInterceptor: NetworkConnectionInterceptor,
         ): RetrofitClient {
 
+            val gson = GsonBuilder().setLenient().create();
+
             val okHttpClient = OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS) // connect timeout
+                .writeTimeout(59, TimeUnit.SECONDS) // write timeout
+                .readTimeout(59, TimeUnit.SECONDS) // read timeout
                 .addInterceptor(networkConnectionInterceptor)
                 .also { client ->
                     if (BuildConfig.DEBUG) {
@@ -46,26 +55,10 @@ interface RetrofitClient {
 
             return Retrofit.Builder()
                 .baseUrl("https://api.kzaman.me/api/")
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(okHttpClient)
                 .build()
                 .create(RetrofitClient::class.java)
-        }
-
-        private fun getRetrofitClient(authenticator: Authenticator? = null): OkHttpClient {
-            return OkHttpClient.Builder()
-                .addInterceptor { chain ->
-                    chain.proceed(chain.request().newBuilder().also {
-                        it.addHeader("Accept", "application/json")
-                    }.build())
-                }.also { client ->
-                    authenticator?.let { client.authenticator(it) }
-                    if (BuildConfig.DEBUG) {
-                        val logging = HttpLoggingInterceptor()
-                        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-                        client.addInterceptor(logging)
-                    }
-                }.build()
         }
     }
 }
